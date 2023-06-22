442d9941c409c604b19933f36e0037cf1e29a895
nandan vasista <nandan@nandan.local>
2023-06-22 18:26:39 +0530

    init: next.js ESLint TailwindCSS

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <main className="bg-black-50">Hello World</main>;
+}
