a4d383a5a658b614628a1a36bb6e511b0c80e370
nandan vasista <nandan@nandan.local>
2023-06-22 19:15:39 +0530

    db schema ✅

--- db/dbscripts.sql
+++ db/dbscripts.sql
@@ -1,12 +1,39 @@
 create database creater_content;
 use  creater_content;
-
-CREATE TABLE `customer_worker`.`users` (
-  `user_id` INT NOT NULL,
+drop table `creater_content`.`users`;
+CREATE TABLE `creater_content`.`users` (
+  `user_id` VARCHAR(45) NOT NULL,
   `username` VARCHAR(45) NOT NULL,
   `email` VARCHAR(55) NOT NULL,
   `paid` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`user_id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
 
-select * from `customer_worker`.`users`;
+select * from `creater_content`.`users`;
+
+CREATE TABLE `creater_content`.`videos` (
+  `video_id` VARCHAR(45) NOT NULL,
+  `title` VARCHAR(100) NOT NULL Unique,
+  `description` VARCHAR(255) NOT NULL,
+  `likes` BIGINT NOT NULL DEFAULT 0,
+  `dis_likes` BIGINT NOT NULL DEFAULT 0,
+  `video_links` MEDIUMTEXT,
+  `uploader_id` VARCHAR(45),
+  PRIMARY KEY (`video_id`),
+  FOREIGN KEY (`uploader_id`) REFERENCES `creater_content`.`users` (`user_id`) ON DELETE SET NULL
+);
+
+create table  `creater_content`.`comments`(
+`user_id` VARCHAR(45) NOT NULL,
+`video_id` VARCHAR(45) NOT NULL,
+`comment_id` VARCHAR(45) NOT NULL,
+`likes` BIGINT NOT NULL DEFAULT 0,
+`dis_likes` BIGINT NOT NULL DEFAULT 0,
+PRIMARY KEY (`user_id`,`video_id`,`comment_id`),
+FOREIGN KEY (`user_id`) REFERENCES `creater_content`.`users` (`user_id`) ON DELETE  CASCADE,
+FOREIGN KEY (`video_id`) REFERENCES `creater_content`.`videos` (`video_id`) ON DELETE  CASCADE
+);
+
+
+select * from comments;
+
