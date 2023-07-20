1c441a7c9f2141e815229b65c72a6e562e06a9e6
nandan vasista <nandan@nandan.local>
2023-07-20 15:45:39 +0530

    feat : dashboard for scripts

--- db/dbscripts.sql
+++ db/dbscripts.sql
@@ -1,6 +1,5 @@
 create database creater_content;
 use  creater_content;
-drop table `creater_content`.`users`;
 CREATE TABLE `creater_content`.`users` (
   `user_id` VARCHAR(45) NOT NULL,
   `username` VARCHAR(45) NOT NULL,
@@ -9,25 +8,18 @@ CREATE TABLE `creater_content`.`users` (
   PRIMARY KEY (`user_id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
 
-truncate `creater_content`.`users`;
-truncate `creater_content`.`videos`;
-truncate  `creater_content`.`comments`;
-select * from `creater_content`.`videos`;
-drop table videos;
-drop table comments;
-CREATE TABLE `creater_content`.`videos` (
+  CREATE TABLE `creater_content`.`videos` (
   `video_id` VARCHAR(45) NOT NULL,
   `title` VARCHAR(100) NOT NULL Unique,
   `description` VARCHAR(255) NOT NULL,
   `likes` BIGINT NOT NULL DEFAULT 0,
   `dis_likes` BIGINT NOT NULL DEFAULT 0,
   `view_count` BIGINT NOT NULL DEFAULT 0,
-  `video_links` MEDIUMTEXT,
+  `video_links` MEDIUMTEXT,--this has to be removed 
   `uploader_id` VARCHAR(45),
   PRIMARY KEY (`video_id`),
   FOREIGN KEY (`uploader_id`) REFERENCES `creater_content`.`users` (`user_id`) ON DELETE SET NULL
 );
-select * from videos;
 
 create table  `creater_content`.`comments`(
 `user_id` VARCHAR(45) NOT NULL,
@@ -39,15 +31,6 @@ PRIMARY KEY (`user_id`,`video_id`,`comment_id`),
 FOREIGN KEY (`user_id`) REFERENCES `creater_content`.`users` (`user_id`) ON DELETE  CASCADE,
 FOREIGN KEY (`video_id`) REFERENCES `creater_content`.`videos` (`video_id`) ON DELETE  CASCADE
 );
-
-
-select * from comments;
-select * from videos;
-truncate videos;
-
-drop table teams;
-drop table team_members;
-
 CREATE TABLE `creater_content`.`teams` (
   `team_id` INT NOT NULL AUTO_INCREMENT,
   `team_name` VARCHAR(100) NOT NULL,
@@ -65,12 +48,6 @@ CREATE TABLE `creater_content`.`team_members` (
   FOREIGN KEY (`team_id`) REFERENCES `creater_content`.`teams` (`team_id`) ON DELETE CASCADE,
   FOREIGN KEY (`user_id`) REFERENCES `creater_content`.`users` (`user_id`) ON DELETE CASCADE
 );
-
-
-use creater_content;
-drop table documents;
-
-
 create table documents (
     doc_id varchar(45) primary key,
     title varchar(255),
@@ -79,11 +56,21 @@ create table documents (
     updated_at datetime default NOW()
 );
 
-create table document_users (
-    `user_id` VARCHAR(45) not null,
-    `doc_id` VARCHAR(45) not null,
-    access tinyint default 0,
+CREATE TABLE document_users (
+    `user_id` VARCHAR(45) NOT NULL,
+    `doc_id` VARCHAR(45) NOT NULL,
+    `access` ENUM('-1', '0', '1', '2') DEFAULT '0',
     PRIMARY KEY (`user_id`, `doc_id`),
-    FOREIGN KEY (`user_id`) REFERENCES `creater_content`.`users` (`user_id`) ON DELETE cascade,
-    FOREIGN KEY (`doc_id`) REFERENCES `creater_content`.`documents` (`doc_id`) ON DELETE cascade
+    FOREIGN KEY (`user_id`) REFERENCES `creater_content`.`users` (`user_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`doc_id`) REFERENCES `creater_content`.`documents` (`doc_id`) ON DELETE CASCADE
 );
+
+drop table `creater_content`.`users`;
+truncate `creater_content`.`users`;
+truncate `creater_content`.`videos`;
+truncate  `creater_content`.`comments`;
+drop table teams;
+drop table team_members;
+drop table documents;
+
+
--- pages/scripts/dashboard.jsx
+++ pages/scripts/dashboard.jsx
@@ -1,24 +1,113 @@
 import { v4 as uuid } from "uuid";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
 
 const Script = () => {
-  const doc_id = uuid();
+  const [newScriptName, setNewScriptName] = useState("");
+  const [listOfScripts, setListOfScripts] = useState([]);
+  const [showOldScripts, setShowOldScripts] = useState(false);
   const router = useRouter();
 
-  const createNewDoc = () => {
-    router.push("/scripts/" + doc_id);
+  const createNewDoc = async (e) => {
+    const doc_id = uuid();
+    e.preventDefault();
+    const user_id = localStorage.getItem("creater-content-user_id");
+    if (!user_id) router.push("/auth/login");
+    try {
+      const res = await axios.post("/api/scripts/save-or-create-script", {
+        title: newScriptName,
+        doc_id,
+        content: "<h1>namaskara World</h1>",
+      });
+      const res2 = await axios.post("/api/scripts/add-user", {
+        doc_id,
+        user_id,
+        access: 2,
+      });
+      //owner access enum[-1,0,1,2] 2 is owner 1 is editor 0 is viewer -1 no access
+      router.push("/scripts/" + doc_id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const loadOldScripts = async () => {
+    const user_id = localStorage.getItem("creater-content-user_id");
+    if (!user_id) router.push("/auth/login");
+    try {
+      const res = await axios.get(
+        "/api/scripts/get-scripts-user?user_id=" + user_id
+      );
+      setListOfScripts(res.data.data);
+      setShowOldScripts(true);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div>
-      {/* in future implement list of all the docs of the user */}
-      <button
-        className="bg-blue-600 p-4 m-4 text-white rounded-md"
-        onClick={createNewDoc}
-      >
-        NEW SCRIPT DOC
-      </button>
+      <h1>Script Dashboard</h1>
+      <form>
+        <input
+          value={newScriptName}
+          placeholder="enter the title of script"
+          onChange={(e) => setNewScriptName(e.target.value)}
+        />
+        <button
+          className="bg-blue-600 p-4 m-4 text-white rounded-md"
+          onClick={(e) => createNewDoc(e)}
+        >
+          NEW SCRIPT DOC
+        </button>
+      </form>
+      <div>
+        {!showOldScripts ? (
+          <button onClick={loadOldScripts}>View All</button>
+        ) : (
+          <button onClick={() => setShowOldScripts(false)}>Close</button>
+        )}
+        {showOldScripts && (
+          <section className=" bg-blue-50">
+            {listOfScripts.map((script) => (
+              <Link
+                href={`/scripts/${script.doc_id}`}
+                key={script.doc_id}
+                className="p-2 m-2"
+              >
+                <h2>{script.title}</h2>
+                <h3>{timeAgo(script.created_at)} </h3>
+                <h3>{timeAgo(script.updated_at)} </h3>
+              </Link>
+            ))}
+          </section>
+        )}
+      </div>
     </div>
   );
 };
+
+export function timeAgo(timestamp) {
+  const currentTime = new Date();
+  const givenTime = new Date(timestamp);
+  const timeDifference = currentTime - givenTime;
+
+  // Convert milliseconds to seconds
+  const seconds = Math.floor(timeDifference / 1000);
+
+  // Calculate time ago
+  if (seconds < 60) {
+    return seconds + "s ago";
+  } else if (seconds < 3600) {
+    const minutes = Math.floor(seconds / 60);
+    return minutes + "m ago";
+  } else if (seconds < 86400) {
+    const hours = Math.floor(seconds / 3600);
+    return hours + "h ago";
+  } else {
+    const days = Math.floor(seconds / 86400);
+    return days + "d ago";
+  }
+}
 
 export default Script;
