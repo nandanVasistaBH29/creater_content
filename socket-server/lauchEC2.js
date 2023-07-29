23f7fca9475b897f98fe8e826c562f82b478c92d
nandan vasista <nandan@nandan.local>
2023-07-29 16:11:57 +0530

    moving launch ec2 to right position

--- socket-server/lauchEC2.js
+++ socket-server/lauchEC2.js
@@ -1,22 +1,21 @@
 // Load the AWS SDK for Node.js
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
+require("dotenv").config();
+
 require("dotenv").config();
 // Load credentials and set region from JSON file
 AWS.config.update({
-  accessKeyId: "",
-  secretAccessKey: "/KGee",
-  region: "ap-south-1",
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: secretAccessKey.region,
 });
-console.clear();
 
-// Create EC2 service object
-var ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
+const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
 
-// AMI is amzn-ami-2011.09.1.x86_64-ebs
-var instanceParams = {
+const instanceParams = {
   ImageId: "ami-0f5ee92e2d63afc18",
   InstanceType: "t2.micro",
-  KeyName: "ec2_key",
+  KeyName: process.env.KeyName,
   MinCount: 1,
   MaxCount: 1,
   UserData: Buffer.from(
@@ -28,14 +27,14 @@ var instanceParams = {
   ).toString("base64"), // Convert the user data to Base64 encoding
 };
 // Create a promise on an EC2 service object and then handle it
-var instancePromise = new AWS.EC2({ apiVersion: "2016-11-15" })
+const instancePromise = new AWS.EC2({ apiVersion: "2016-11-15" })
   .runInstances(instanceParams)
   .promise();
 
 instancePromise
   .then(function (data) {
     console.log(data);
-    var instanceId = data.Instances[0].InstanceId;
+    const instanceId = data.Instances[0].InstanceId;
     console.log("Created instance", instanceId);
     // Add tags to the instance
     tagParams = {
@@ -48,7 +47,7 @@ instancePromise
       ],
     };
     // Create a promise on an EC2 service object
-    var tagPromise = new AWS.EC2({ apiVersion: "2016-11-15" })
+    const tagPromise = new AWS.EC2({ apiVersion: "2016-11-15" })
       .createTags(tagParams)
       .promise();
 
